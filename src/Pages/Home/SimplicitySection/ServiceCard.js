bba5136d8c6fd33d3b143621695b827954d7cf6c
asifilbal01890 <asifikbal01890@gmail.com>
2023-05-14 11:18:07 +0600

    button fixing

--- src/Pages/Home/SimplicitySection/ServiceCard.js
+++ src/Pages/Home/SimplicitySection/ServiceCard.js
@@ -14,7 +14,7 @@ const ServiceCard = ({ cardInfo }) => {
                     <h1 className='text-[28px] font-bold leading-[34px]'>{title}</h1>
                     <p className='text-sm leading-[17px] px-1 pt-[13px]'>{description}</p>
                 </div>
-                <button className={`${colorOfBtnContent} w-full font-semibold text-sm py-[13px] px-[39px] bg-[#2E2D32] rounded-[40px] mt-[10px]`} >{contentOfButton}</button>
+                <button className={`${colorOfBtnContent} w-full font-semibold text-sm py-[13px] px-[39px] bg-[#2E2D32] hover:bg-[#52C1B9] hover:text-white duration-300 rounded-[40px] mt-[10px]`} >{contentOfButton}</button>
             </div>
         </div>
     );
